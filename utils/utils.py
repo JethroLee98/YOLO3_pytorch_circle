d22b75d67193153fe33c76eb870792c2b68d5b06
Lingxiao Li <lingxiaoli98@gmail.com>
2022-05-12 15:48:42 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,6 @@
 import numpy as np
 from PIL import Image
 
-#---------------------------------------------------------#
-#   将图像转换成RGB图像，防止灰度图在预测时报错。
-#   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-#---------------------------------------------------------#
 def cvtColor(image):
     if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
         return image 
@@ -12,9 +8,6 @@ def cvtColor(image):
         image = image.convert('RGB')
         return image 
     
-#---------------------------------------------------#
-#   对输入图像进行resize
-#---------------------------------------------------#
 def resize_image(image, size, letterbox_image):
     iw, ih  = image.size
     w, h    = size
@@ -30,18 +23,12 @@ def resize_image(image, size, letterbox_image):
         new_image = image.resize((w, h), Image.BICUBIC)
     return new_image
 
-#---------------------------------------------------#
-#   获得类
-#---------------------------------------------------#
 def get_classes(classes_path):
     with open(classes_path, encoding='utf-8') as f:
         class_names = f.readlines()
     class_names = [c.strip() for c in class_names]
     return class_names, len(class_names)
 
-#---------------------------------------------------#
-#   获得先验框
-#---------------------------------------------------#
 def get_anchors(anchors_path):
     '''loads the anchors from a file'''
     with open(anchors_path, encoding='utf-8') as f:
@@ -50,9 +37,6 @@ def get_anchors(anchors_path):
     anchors = np.array(anchors).reshape(-1, 2)
     return anchors, len(anchors)
 
-#---------------------------------------------------#
-#   获得学习率
-#---------------------------------------------------#
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
